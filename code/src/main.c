8037889f28dfb7a1746325f557f7f6acc1c1eae5
GuavTek <36814224+GuavTek@users.noreply.github.com>
2025-09-22 19:05:57 +0200

    Add step velocity

--- code/src/main.c
+++ code/src/main.c
@@ -14,12 +14,17 @@ uint32_t blink_time = 1000000;
 int16_t current_bpm = 120;
 uint64_t current_uspb = 60000000/120; // µs per beat
 uint8_t current_playing = 0;
-bool steps[NUM_CHANNELS][16];
+int8_t steps[NUM_CHANNELS][16];
 uint8_t current_idx[NUM_CHANNELS];
 uint8_t max_idx[NUM_CHANNELS];
+uint8_t velocities[4];
 bool step_state;
 
+void clamp_uint8(uint8_t* target, int32_t delta);
 void midi_rx();
+void led_set_step(uint8_t row, uint8_t col, int8_t vel);
+void led_set_end(uint8_t row, uint8_t col);
+void renc_change(int32_t delta);
 
 // TODO: sequencer
 int main() {
@@ -40,7 +45,11 @@ int main() {
     for (uint8_t i = 0; i < NUM_CHANNELS; i++){
         max_idx[i] = 15;
     }
-    
+
+    led_set_step(0, 0, velocities[0]);
+    led_set_step(0, 1, velocities[1]);
+    led_set_step(1, 0, velocities[2]);
+    led_set_step(1, 1, velocities[3]);
 
     while (true) {
         static uint64_t step_time = 0;
@@ -49,7 +58,7 @@ int main() {
             // We have passed end of time?!
         }
         prev_time = time_us_64();
-        
+
         // Leds: 30ms
         static uint64_t led_time = 0;
         if (led_time < prev_time){
@@ -72,19 +81,15 @@ int main() {
                 } else {
                     // Direction changed, reset mult
                     renc_mult = 0x200;
+                    renc_dir = renc_increment > 0;
                 }
-                renc_dir = renc_increment > 0;
-                current_bpm += renc_increment * (renc_mult >> 9);
-                if (current_bpm < 10){
-                    current_bpm = 10;
-                }
-                current_uspb = 60000000 / current_bpm;
+                renc_change(renc_increment * (renc_mult >> 9));
                 renc_increment = 0;
             }
             if (renc_mult > 0){
                 renc_mult -= 1 + (renc_mult >> 9);
             }
-            
+
             // Check buttons
             static bool prev_butt[80];
             for (uint8_t i = 0; i < 80; i++){
@@ -99,13 +104,13 @@ int main() {
                             // Clear step led
                             for (uint8_t j = 0; j < 4; j++){
                                 uint8_t idx = current_idx[j];
-                                led_set(j+2, idx, steps[j][idx]*64, 0, 0);
+                                led_set_step(j+2, idx, steps[j][idx]);
                                 if (idx == 0){
                                     idx = max_idx[j];
                                 } else {
                                     idx--;
                                 }
-                                led_set(j+2, idx, steps[j][idx]*64, 0, 0);
+                                led_set_step(j+2, idx, steps[j][idx]);
                             }
                             for (uint8_t j = 0; j < NUM_CHANNELS; j++){
                                 // Send note offs
@@ -127,22 +132,36 @@ int main() {
                         r = (i >> 4)-1;
                         c = i & 0b1111;
                         if (butts[15]){
+                            // Holding button 15 selects sequence end
                             // Clear the previous end LED
-                            led_set(r+2, max_idx[r]+1, steps[r][max_idx[r]+1] * 64, 0, 0);
+                            led_set_step(r+2, max_idx[r]+1, steps[r][max_idx[r]+1]);
                             // Set new end step
                             max_idx[r] = c;
-                            led_set(r+2, c+1, 0, 0, 64);
+                            led_set_end(r+2, c+1);
+                            // TODO? maybe it should complete the previous length
                             // Reset pointer if it has passed the new end step
                             if (current_idx[r] > c){
                                 current_idx[r] = 0;
                             }
                             // Reset LEDs after new end step
                             for (uint8_t i = c+2; i < 16; i++){
-                                led_set(r+2, i, steps[r][i]*64, 0, 0);
+                                led_set_step(r+2, i, steps[r][i]);
                             }
                         } else {
-                            steps[r][c] = !steps[r][c];
-                            led_set(r+2, c, steps[r][c] * 64, 0, 0);
+                            // Set new velocity on step?
+                            if (butts[0]) {
+                                steps[r][c] = velocities[0];
+                            } else if (butts[1]) {
+                                steps[r][c] = velocities[1];
+                            } else if (butts[8]) {
+                                steps[r][c] = velocities[2];
+                            } else if (butts[9]) {
+                                steps[r][c] = velocities[3];
+                            } else {
+                                // Default toggling previous velocity on or off
+                                steps[r][c] = -steps[r][c]-1;
+                            }
+                            led_set_step(r+2, c, steps[r][c]);
                         }
                     }
                 }
@@ -160,13 +179,13 @@ int main() {
             if (div_step == 0){
                 for (uint8_t i = 0; i < 4; i++){
                     uint8_t c = max_idx[i]+1;
-                    if (!steps[i][c]) continue;
-                    led_set(i+2, c, 64, 0, 0);
+                    if (steps[i][c] < 0) continue;
+                    led_set_step(i+2, c, steps[i][c]);
                 }
             } else if (div_step == 0x8){
                 for (uint8_t i = 0; i < 4; i++){
                     uint8_t c = max_idx[i]+1;
-                    led_set(i+2, c, 0, 0, 64);
+                    led_set_end(i+2, c);
                 }
             }
             if (step_state){
@@ -176,7 +195,7 @@ int main() {
                     // Update LED matrix
                     for(uint8_t i = 0; i < 4; i++){
                         uint8_t idx = current_idx[i];
-                        bool active = steps[i][idx];
+                        bool active = steps[i][idx] >= 0;
                         if(active){
                             led_set(i+2, idx, 0, 63, 63);
                         } else {
@@ -192,17 +211,17 @@ int main() {
                         } else {
                             idx--;
                         }
-                        led_set(i+2, idx, steps[i][idx]*64, 0, 0);
+                        led_set_step(i+2, idx, steps[i][idx]);
                     }
                     // Send midi note on
                     if (tud_midi_mounted()){
                         uint8_t buff[3*NUM_CHANNELS];
                         uint8_t len = 0;
                         for (uint8_t i = 0; i < NUM_CHANNELS; i++){
-                            if (steps[i][current_idx[i]]){
-                                buff[len++] = 0x99;
-                                buff[len++] = 0x20+i;
-                                buff[len++] = 0x40;
+                            if (steps[i][current_idx[i]] >= 0){
+                                buff[len++] = 0x99;     // Note on
+                                buff[len++] = 0x20+i;   // key
+                                buff[len++] = steps[i][current_idx[i]] & 0x7f;     // velocity
                             }
                             tud_midi_stream_write(0,buff, len);
                         }
@@ -217,15 +236,15 @@ int main() {
                         uint8_t buff[3*NUM_CHANNELS];
                         uint8_t len = 0;
                         for (uint8_t i = 0; i < NUM_CHANNELS; i++){
-                            if(steps[i][current_idx[i]]){
-                                buff[len++] = 0x89;
-                                buff[len++] = 0x20+i;
-                                buff[len++] = 0x0;
+                            if(steps[i][current_idx[i]] >= 0){
+                                buff[len++] = 0x89;     // Note off
+                                buff[len++] = 0x20+i;   // key
+                                buff[len++] = 0x0;      // velocity
                             }
                         }
                         tud_midi_stream_write(0, buff, len);
                     }
-                    for (uint8_t i = 0; i < NUM_CHANNELS; i++){                        
+                    for (uint8_t i = 0; i < NUM_CHANNELS; i++){
                         current_idx[i]++;
                         if (current_idx[i] > max_idx[i]){
                             current_idx[i] = 0;
@@ -245,6 +264,61 @@ int main() {
         tud_task();
         midi_rx();
 
+    }
+}
+
+void clamp_uint8(uint8_t* target, int32_t delta){
+    if (*target + delta > 255) {
+        *target = 255;
+    } else if (*target + delta < 0) {
+        *target = 0;
+    } else {
+        *target += delta;
+    }
+}
+
+void led_set_step(uint8_t row, uint8_t col, int8_t vel){
+    bool active = vel >= 0;
+    if (active){
+        led_set(row, col, 64, vel, 0);
+    } else {
+        led_set(row, col, 0, 0, 0);
+    }
+}
+
+
+void led_set_end(uint8_t row, uint8_t col){
+    led_set(row, col, 0, 0, 64);
+}
+
+void renc_change(int32_t delta){
+    if (butts[0]) {
+        // Change velocity preset
+        clamp_uint8(&velocities[0], delta);
+        velocities[0] &= 0x7f;
+        led_set_step(0, 0, velocities[0]);
+    } else if (butts[1]) {
+        // Change velocity preset
+        clamp_uint8(&velocities[1], delta);
+        velocities[1] &= 0x7f;
+        led_set_step(0, 1, velocities[1]);
+    } else if (butts[8]) {
+        // Change velocity preset
+        clamp_uint8(&velocities[2], delta);
+        velocities[2] &= 0x7f;
+        led_set_step(1, 0, velocities[2]);
+    } else if (butts[9]) {
+        // Change velocity preset
+        clamp_uint8(&velocities[3], delta);
+        velocities[3] &= 0x7f;
+        led_set_step(1, 1, velocities[3]);
+    } else {
+        // Default to changing tempo
+        current_bpm += delta;
+        if (current_bpm < 10){
+            current_bpm = 10;
+        }
+        current_uspb = 60000000 / current_bpm;
     }
 }
 
